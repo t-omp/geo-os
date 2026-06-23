0605b2dea604de418dad35c6153e3018771b6be3
t-omp <93682204+t-omp@users.noreply.github.com>
2026-06-23 04:21:40 +0100

    fixed path

--- files/scripts/emacs-bedrock.sh
+++ files/scripts/emacs-bedrock.sh
@@ -9,14 +9,15 @@ set -oue pipefail
 rm -rf /tmp/emacs-bedrock
 git clone https://codeberg.org/ashton314/emacs-bedrock.git /tmp/emacs-bedrock
 echo "(set-frame-parameter nil 'fullscreen 'fullboth)" >> /tmp/emacs-bedrock/init.el
-touch /tmp/.xinitrc
-echo "exec emacs" >> /tmp/.xinitrc
+cat > /tmp/.xinitrc <<'EOF'
+exec emacs
+EOF
 # Install into /etc/skel so new users get the config
 mkdir -p /etc/skel/.emacs.d
 cp /tmp/emacs-bedrock/early-init.el /etc/skel/.emacs.d/
 cp /tmp/emacs-bedrock/init.el /etc/skel/.emacs.d/
 cp -r /tmp/emacs-bedrock/extras /etc/skel/.emacs.d/
-cp /tmp/emacs-bedrock/.xinitrc /etc/skel/.xinitrc
+cp /tmp/.xinitrc /etc/skel/.xinitrc
 
 # Clean up
 rm -rf /tmp/emacs-bedrock
