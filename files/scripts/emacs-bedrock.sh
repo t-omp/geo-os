6d51fa9396f32083cd8522799c2ec9ae505ab5dd
t-omp <93682204+t-omp@users.noreply.github.com>
2026-06-23 04:07:50 +0100

    xinitrc

--- files/scripts/emacs-bedrock.sh
+++ files/scripts/emacs-bedrock.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-
+set -oue pipefail
 # Tell this script to exit if there are any errors.
 # You should have this in every custom script, to ensure that your completed
 # builds actually ran successfully without any errors!
-set -oue pipefail
+
 
 # Clone emacs-bedrock from the repository
 rm -rf /tmp/emacs-bedrock
 git clone https://codeberg.org/ashton314/emacs-bedrock.git /tmp/emacs-bedrock
 echo "(set-frame-parameter nil 'fullscreen 'fullboth)" >> /tmp/emacs-bedrock/init.el
-echo "exec emacs" > ~/tmp/.xinitrc
+touch /tmp/.xinitrc
+echo "exec emacs" >> /tmp/.xinitrc
 # Install into /etc/skel so new users get the config
 mkdir -p /etc/skel/.emacs.d
 cp /tmp/emacs-bedrock/early-init.el /etc/skel/.emacs.d/
